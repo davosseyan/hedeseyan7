5791698eb3af963e2c64cce943a41e404520173c
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-03-17 12:05:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help | Server ${HARDE UP}', { type: "LISTENING" });
+  client.user.setActivity(`${PREFIX}help | server ${client.guilds.cache.size}`, { type: "PLAYING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
